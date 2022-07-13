e74d81f383b6d85a54d814612b1a6d47055ae01b
pctripsesp <pctripsesp@users.noreply.github.com>
2022-07-13 20:41:23 +0200

    Create get_log_time.sh

---
+++ get_log_time.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## ./get_log_time.sh /tmp/repositories.txt 2>/dev/null
+
+
+for REPO in `cat $1`; do
+
+    # DOWNLOAD REPO
+    git clone $REPO
+    
+    folderName=$(echo $REPO | cut -d/ -f5- | cut -d. -f1)
+    cd $folderName*
+
+    # GET LOGS
+    filename='/tmp/OUTPUT.txt'
+    echo $REPO >> $filename
+    git log | grep Date >> $filename
+
+    # RM repositories
+    cd ..
+    rm -rf $folderName* 
+done
