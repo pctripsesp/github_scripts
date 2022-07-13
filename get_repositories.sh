2889317fd18ddc7f099a1f1f7e408217c26ce3af
pctripsesp <pctripsesp@users.noreply.github.com>
2022-07-13 13:56:23 +0200

    Update get_repositories.sh
    fixed missing 0 in array

--- get_repositories.sh
+++ get_repositories.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-## ./get_github.sh /home/eddy/github_usernames.txt 2>/dev/null
+## ./get_repositories.sh /home/teddy/usernames.txt 2>/dev/null
+# You want to login to increase api request limit
+#export GITHUB_USER=<USERNAME>
+#export GITHUB_PASSWORD=<PASSWORD>
 
 for USERNAME in `cat $1`; do
 
     # GET RAW JSON DATA
-    json=$(curl https://api.github.com/users/$USERNAME/repos)
+    json=$(curl -X GET -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/users/$USERNAME/repos)
 
     # GET NUMBER OF REPOS
     n=$(echo "$json" | jq -r '. | length')
     n=$((n-1))
-    
+
     # GET REPO NAME
-    for i in $( seq 1 $n )
+    for i in $( seq 0 $n )
         do
             url_repo=$(echo "$json" | jq -r .[$i].html_url)
             # PRINT ALL REPOS
