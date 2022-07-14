d362d1ce6142c2b2065a2a1cdbb24c83624d2f2b
pctripsesp <pctripsesp@users.noreply.github.com>
2022-07-14 13:22:44 +0200

    Update get_log_time.sh
    now it takes only commits from forks that this user made

--- get_log_time.sh
+++ get_log_time.sh
@@ -8,13 +8,14 @@ for REPO in `cat $1`; do
     # DOWNLOAD REPO
     git clone $REPO
     
+    USERNAME=$(echo $REPO | cut -d/ -f4)
     folderName=$(echo $REPO | cut -d/ -f5- | cut -d. -f1)
     cd $folderName*
 
     # GET LOGS
     filename='/tmp/OUTPUT.txt'
     echo $REPO >> $filename
-    git log | grep Date >> $filename
+    git log |grep -A1 $USERNAME |grep 'Date' >> $filename
 
     # RM repositories
     cd ..
